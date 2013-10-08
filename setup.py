2f9c79c7f768e17f3b312a7aa3b09b3d2c8374af
John Evans <john.g.evans.ne@gmail.com>
2013-10-08 12:07:53 -0400

    pylint work

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2008-2009, European Space Agency & European Southern 
+# Copyright (c) 2008-2009, European Space Agency & European Southern
 # Observatory (ESA/ESO)
-# Copyright (c) 2008-2009, CRS4 - Centre for Advanced Studies, Research and 
+# Copyright (c) 2008-2009, CRS4 - Centre for Advanced Studies, Research and
 # Development in Sardinia
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
-# 
+#
 #     * Redistributions in binary form must reproduce the above copyright
 #       notice, this list of conditions and the following disclaimer in the
 #       documentation and/or other materials provided with the distribution.
-# 
-#     * Neither the name of the European Space Agency, European Southern 
+#
+#     * Neither the name of the European Space Agency, European Southern
 #       Observatory, CRS4 nor the names of its contributors may be used to
 #       endorse or promote products derived from this software without specific
 #       prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY ESA/ESO AND CRS4 ``AS IS'' AND ANY EXPRESS OR
 # IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 # MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
@@ -40,29 +40,31 @@ try:
     from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup, find_packages
-import sys
 
-def read_version():
-    try:
-        return open('VERSION', 'r').readline().strip()
-    except IOError as e:
-        raise SystemExit(
-            "Error: you must run setup from the root directory (%s)" % str(e))
+try:
+    VERSION = open('VERSION', 'r').readline().strip()
+except IOError as err:
+    MSG = "Error: you must run setup from the root directory ({0})"
+    MSG = MSG.format(err)
+    raise SystemExit(MSG)
 
-long_desc = "Python XMP Toolkit is a library for working with XMP metadata, "
-long_desc += "as well as reading/writing XMP metadata stored in many "
-long_desc += "different file formats."
+LONG_DESC = "Python XMP Toolkit is a library for working with XMP metadata, "
+LONG_DESC += "as well as reading/writing XMP metadata stored in many "
+LONG_DESC += "different file formats."
 
+DOWNLOAD_URL = 'http://code.google.com/p/python-xmp-toolkit/downloads/list'
 
-setup(
-    name='python-xmp-toolkit',
-    version=read_version(),
-    description='Python XMP Toolkit for working with metadata.',
-    author='Lars Holm Nielsen, Federico Caboni & Amit Kapadia',
-    author_email='lnielsen@eso.org,federico.caboni@me.com,akapad@gmail.com',
-    url='http://code.google.com/p/python-xmp-toolkit/',
-    download_url='http://code.google.com/p/python-xmp-toolkit/downloads/list',
-    long_description=long_desc,
-    license='New BSD License',
-    packages=find_packages(exclude=["*test*"])
-)
+KWARGS = {
+    'name': 'python-xmp-toolkit',
+    'description': 'Python XMP Toolkit for working with metadata.',
+    'author': 'Lars Holm Nielsen, Federico Caboni & Amit Kapadia',
+    'author_email': 'lnielsen@eso.org,federico.caboni@me.com,akapad@gmail.com',
+    'url': 'http://code.google.com/p/python-xmp-toolkit/',
+    'long_description': LONG_DESC,
+    'download_url': DOWNLOAD_URL,
+    'license': 'New BSD License',
+    'packages': find_packages(exclude=["*test*"]),
+    'version': VERSION
+}
+
+setup(**KWARGS)
