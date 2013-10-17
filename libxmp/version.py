dc1124cc45e5431d3aab83e8e341ef3a9cf4437b
John Evans <john.g.evans.ne@gmail.com>
2013-10-17 06:38:34 -0400

    Moved version information directly into package.
    Opportunity to interrogate libexempi for version information in the future.  #2

--- libxmp/version.py
+++ libxmp/version.py
@@ -32,41 +32,11 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 
-"""
-Install script for libxmp.
-"""
-import os
-import re
+import sys
+from distutils.version import LooseVersion
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup, find_packages
-
-LONG_DESC = "Python XMP Toolkit is a library for working with XMP metadata, "
-LONG_DESC += "as well as reading/writing XMP metadata stored in many "
-LONG_DESC += "different file formats."
-
-DOWNLOAD_URL = 'http://code.google.com/p/python-xmp-toolkit/downloads/list'
-
-KWARGS = {
-    'name': 'python-xmp-toolkit',
-    'description': 'Python XMP Toolkit for working with metadata.',
-    'author': 'Lars Holm Nielsen, Federico Caboni & Amit Kapadia',
-    'author_email': 'lnielsen@eso.org,federico.caboni@me.com,akapad@gmail.com',
-    'url': 'http://code.google.com/p/python-xmp-toolkit/',
-    'long_description': LONG_DESC,
-    'download_url': DOWNLOAD_URL,
-    'license': 'New BSD License',
-    'install_requires': ['pytz', 'flufl.enum>=4.0'],
-    'packages': find_packages(exclude=["*test*"]),
-}
-
-# Get the version string.  Cannot do this by importing glymur!
-version_file = os.path.join('libxmp', 'version.py')
-with open(version_file, 'rt') as fptr:
-    contents = fptr.read()
-    match = re.search('version\s*=\s*"(?P<version>\d*.\d*.\d*.*)"\n', contents)
-    KWARGS['version'] = match.group('version')
-
-setup(**KWARGS)
+# Do not change the format of this next line!  Doing so risks breaking
+# setup.py
+version = "1.9.0"
+_sv = LooseVersion(version)
+version_tuple = _sv.version
